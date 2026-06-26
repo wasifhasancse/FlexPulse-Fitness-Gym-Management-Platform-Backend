ca17e1a302fb86a02d65f590d3eda026dd3fea45
Wasif Hasan <wasifhasancse@gmail.com>
2026-06-27 01:37:09 +0600

    set Verify admin role middleware

--- index.js
+++ index.js
@@ -64,6 +64,15 @@ const trainerVerify = async (req, res, next) => {
   next();
 };
 
+// Verify admin role middleware
+const adminVerify = async (req, res, next) => {
+  const user = req.user;
+  if (user.role !== "admin") {
+    return res.status(403).json({ msg: "Forbidden" });
+  }
+  next();
+};
+
 const run = async () => {
   try {
     // await client.connect();
