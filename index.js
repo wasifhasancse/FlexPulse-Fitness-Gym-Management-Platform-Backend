38398ab69b8ab1182e9929abac56f924d28caa67
Wasif Hasan <wasifhasancse@gmail.com>
2026-06-26 17:54:00 +0600

    set api route for get all classes and filter by search and category

--- index.js
+++ index.js
@@ -34,9 +34,26 @@ const run = async () => {
     );
     const userCollection = database.collection("user");
 
+    // get all users
     app.get("/api/all-users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
+    });
+
+    // get all classes and filter by search and category
+    app.get("/api/all-class", async (req, res) => {
+      try {
+        const { search = "", category = "" } = req.query;
+        const query = {};
+        if (search) query.className = { $regex: search, $options: "i" };
+        if (category && category !== "All Categories")
+          query.category = category;
+        const result = await classCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).send({ message: error.message });
+      }
     });
 
     // await client.db("admin").command({ ping: 1 });
